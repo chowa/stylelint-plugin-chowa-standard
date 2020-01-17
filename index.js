28281b549670c4e6d1bce9f8b54290726f4dbf50
smoking rabbit <xuecong8226@gmail.com>
2020-01-17 20:14:15 +0800

    configure selector-pseudo-class-no-unknown for css module global

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ module.exports = {
         'no-extra-semicolons': true,
         'no-invalid-double-slash-comments': true,
         'property-no-unknown': true,
-        'selector-pseudo-class-no-unknown': true,
+        'selector-pseudo-class-no-unknown': [
+            true,
+            {
+                'ignorePseudoClasses': [
+                    'global'
+                ]
+            }
+        ],
         'selector-pseudo-element-no-unknown': true,
         'selector-type-no-unknown': true,
         'string-no-newline': true,
